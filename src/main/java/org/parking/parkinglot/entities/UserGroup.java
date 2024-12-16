709042bd066be518e1938d590eb18ba12bfb9794
AVRAM MATEI-ALEXANDRU <avramalexandru2030@gmail.com>
2024-12-16 11:03:14 +0200

    Lab 9 pe jumatate gata

---
+++ src/main/java/org/parking/parkinglot/entities/UserGroup.java
@@ -0,0 +1,38 @@
+package org.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UserGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String username;
+    private String userGroup;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserGroup() {
+        return userGroup;
+    }
+
+    public void setUserGroup(String userGroup) {
+        this.userGroup = userGroup;
+    }
+}
